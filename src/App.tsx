a3148b042e3351935136345b9b95dd39a2aaf94d
BGTYaneck <bezulak.jr@o2.pl>
2023-05-29 14:41:18 +0200

    Complete responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   //true - Graph , False- favourites
   const [isGraph, setIsGraph] = useState(true);
 
+  let date;
+  const today = new Date();
+  date = today.getDate() + ".";
+  today.getMonth() < 9
+    ? (date = date + "0" + (today.getMonth() + 1))
+    : +today.getMonth() + 1;
+  date = date + "." + today.getFullYear();
   return (
     <>
-      <div className="w-full h-[4.5rem] flex justify-center items-center bg-[#101532] border-b-2 border-[#242944]">
-        <button
-          //@ts-ignore
-          className={
-            isGraph
-              ? "border-b-4 p-2 text-white h-full border-[#8C7CF0] lg:w-48 w-1/2 font-bold transition ease-in-out"
-              : "p-2 text-white opacity-60 lg:w-48 w-1/2"
-          }
-          onClick={() => setIsGraph(true)}
-        >
-          Favourites
-        </button>
-        <button
-          //@ts-ignore
-          className={
-            isGraph == false
-              ? "border-b-4 p-2 text-white h-full border-[#8C7CF0] lg:w-48 w-1/2 font-bold transition ease-in-out"
-              : "p-2 text-white opacity-60 lg:w-48 w-1/2"
-          }
-          onClick={() => setIsGraph(false)}
-        >
-          Stocks
-        </button>
+      <div className="w-full h-[4.5rem] flex items-center bg-[#101532] border-b-2 border-[#242944] md:justify-between px-2">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3799/3799817.png"
+          className="h-full p-2 hidden md:block"
+        />
+        <div className="h-full w-full md:w-96">
+          <button
+            //@ts-ignore
+            className={
+              isGraph
+                ? "border-b-4 p-2 text-white h-full border-[#8C7CF0] md:w-48 w-1/2 font-bold transition ease-in-out"
+                : "p-2 text-white opacity-60 md:w-48 w-1/2"
+            }
+            onClick={() => setIsGraph(true)}
+          >
+            Favourites
+          </button>
+          <button
+            //@ts-ignore
+            className={
+              isGraph == false
+                ? "border-b-4 p-2 text-white h-full border-[#8C7CF0] md:w-48 w-1/2 font-bold transition ease-in-out"
+                : "p-2 text-white opacity-60 md:w-48 w-1/2"
+            }
+            onClick={() => setIsGraph(false)}
+          >
+            Stocks
+          </button>
+        </div>
+        <div className="p-2 text-white opacity-40 hidden md:block">{date}</div>
       </div>
     </>
   );
